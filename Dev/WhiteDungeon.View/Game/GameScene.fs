eee9780b31ee0777bf21b0719ed751d2374cf5d0
wraikny <writtenknown@gmail.com>
2019-05-04 21:18:14 +0900

    view

---
+++ Dev/WhiteDungeon.View/Game/GameScene.fs
@@ -0,0 +1,13 @@
+﻿namespace WhiteDungeon.View.Game
+
+open wraikny
+open wraikny.MilleFeuille.Core.Object
+
+open WhiteDungeon.Core
+
+[<Class>]
+type GameScene() =
+    inherit Scene()
+
+    let messenger =
+        Game.Tart.createMessage
