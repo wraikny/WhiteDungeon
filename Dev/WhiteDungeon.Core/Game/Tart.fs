d3fc5e673ec1421a55e8e7c924a27966f069a643
wraikny <writtenknown@gmail.com>
2019-05-04 18:26:51 +0900

    game tart

---
+++ Dev/WhiteDungeon.Core/Game/Tart.fs
@@ -0,0 +1,20 @@
+﻿module WhiteDungeon.Core.Game.Tart
+
+open wraikny.Tart.Core
+open WhiteDungeon.Core.Game
+
+
+let initModel p = Model.Model.init p, Cmd.none
+
+
+let createMessage updater initParam =
+    Messenger.buildMessenger
+        {
+            seed = 0
+            updater = updater
+        }
+        {
+            init = initModel initParam
+            update = Update.Update.update
+            view = ViewModel.ViewModel.view
+        }
