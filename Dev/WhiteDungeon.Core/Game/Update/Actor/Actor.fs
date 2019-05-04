ed261fd0231d40e4672a783fc7491aff0e5b5efc
wraikny <writtenknown@gmail.com>
2019-05-04 15:43:53 +0900

    update definitions wip

---
+++ Dev/WhiteDungeon.Core/Game/Update/Actor/Actor.fs
@@ -0,0 +1,18 @@
+﻿module WhiteDungeon.Core.Game.Update.Actor.Actor
+
+open WhiteDungeon.Core.Game
+
+open WhiteDungeon.Core.Game.Update
+
+
+let setObjectBase (objectBase : Model.ObjectBase) (actor : Model.Actor.Actor) =
+    { actor with objectBase = objectBase }
+
+
+let updateObjectBase (actor : Model.Actor.Actor) =
+    let updatedObjectBase =
+        actor.objectBase
+        |> ObjectBase.update
+
+    actor
+    |> setObjectBase updatedObjectBase
---
+++ Dev/WhiteDungeon.Core/Game/Update/ObjectBase.fs
@@ -0,0 +1,24 @@
+﻿module WhiteDungeon.Core.Game.Update.ObjectBase
+
+
+open WhiteDungeon.Core.Game.Model
+
+let setPosition position (obj : ObjectBase) = {
+    obj with
+        position = position
+        lastPosition = obj.position
+}
+
+
+let addPosition diff (obj : ObjectBase) =
+    obj |> setPosition (diff + obj.position)
+
+
+let setVelocity velocity (obj : ObjectBase) = {
+    obj with
+        velocity = velocity
+}
+
+let update (obj : ObjectBase) =
+    obj
+    |> addPosition obj.velocity
