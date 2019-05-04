5439f367e5f0238e5425cb5676bdff8e6a7f9251
wraikny <writtenknown@gmail.com>
2019-05-04 18:10:47 +0900

    gamesetting module fixed more generaly

--- Dev/WhiteDungeon.Core/Game/Model/GameSetting.fs
+++ Dev/WhiteDungeon.Core/Game/Model/GameSetting.fs
@@ -1,18 +1,17 @@
 ﻿namespace WhiteDungeon.Core.Game.Model
 
 open wraikny.Tart.Helper.Math
+open wraikny.Tart.Helper.Geometry
 
 type GameSetting = {
     dungeonCellSize : float32 Vec2
 }
 
 module GameSetting =
-    let cellToCoordinate (cell : int Vec2) (gameSetting : GameSetting) : float32 Vec2 =
+    let fromDungeonCell (cellSize : float32 Vec2) (cell : int Vec2) : float32 Vec2 =
         let cellf = cell |> Vec2.map float32
-        let cellSize = gameSetting.dungeonCellSize
         Vec2.init(cellf.x * cellSize.x, cellf.y * cellSize.y)
 
-    let coordinateToCell (coordinate : float32 Vec2) (gameSetting : GameSetting) : int Vec2 =
-        let cellSize = gameSetting.dungeonCellSize
+    let toDungeonCell (cellSize : float32 Vec2) (coordinate : float32 Vec2) : int Vec2 =
         Vec2.init(coordinate.x / cellSize.x, coordinate.y / cellSize.y)
         |> Vec2.map int
