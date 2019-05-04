7152d1f3c719ac67b14b2ed7f015544f98788cf3
wraikny <writtenknown@gmail.com>
2019-05-05 00:07:57 +0900

    core main

--- Dev/WhiteDungeon.Core/Main.fs
+++ Dev/WhiteDungeon.Core/Main.fs
@@ -5,11 +5,12 @@ open WhiteDungeon.Core
 open wraikny.Tart.Core
 
 type Model =
-    | PreparationModel
+    | PreparationModel of Preparation.Model
     | GameModel of Game.Model.Model
 
 
 type Msg =
+    | PreparationMsg of Preparation.Msg
     | GameMsg of Game.Msg.Msg
 
 
@@ -18,12 +19,23 @@ type ErrorKind =
 
 
 type ViewMsg =
+    | PreparationViewMsg of Preparation.ViewMsg
     | GameViewMsg of Game.ViewMsg.ViewMsg
     // | Error of ErrorKind
 
 
 let update (msg : Msg) (model : Model) : Model * Cmd<Msg, ViewMsg> =
     (msg, model) |> function
+    | PreparationMsg msg, PreparationModel model ->
+        let model, cmd = Preparation.Update.update msg model
+
+        let cmd =
+            cmd
+            |> Cmd.mapCommands PreparationMsg
+            |> Cmd.mapViewMsgs PreparationViewMsg
+
+        PreparationModel model, cmd
+
     | GameMsg msg, GameModel model ->
         let model, cmd = Game.Update.Update.update msg model
 
@@ -40,14 +52,15 @@ let update (msg : Msg) (model : Model) : Model * Cmd<Msg, ViewMsg> =
 
 
 type ViewModel =
-    | Pseudo
+    | PreparationViewModel of Preparation.ViewModel
     | GameViewModel of Game.ViewModel.ViewModel
 
 
 let view (model : Model) : ViewModel =
     model |> function
-    | PreparationModel ->
-        Pseudo
+    | PreparationModel model ->
+        Preparation.ViewModel.view model
+        |> PreparationViewModel
 
     | GameModel model ->
         Game.ViewModel.ViewModel.view model
@@ -55,18 +68,18 @@ let view (model : Model) : ViewModel =
 
 
 
-let init() =
-    PreparationModel, Cmd.none
+let init pModel =
+    PreparationModel pModel, Cmd.none
 
 
-let createMessenger (seed, updater) =
+let createMessenger (seed, updater) pModel =
     Messenger.buildMessenger
         {
             seed = seed
             updater = Some updater
         }
         {
-            init = init()
+            init = init pModel
             update = update
             view = view
         }
