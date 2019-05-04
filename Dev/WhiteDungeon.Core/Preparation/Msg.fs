38ea5a085d80b37b5cd03cb7ff90f2ff743cd573
wraikny <writtenknown@gmail.com>
2019-05-04 22:59:00 +0900

    preparation msg update

---
+++ Dev/WhiteDungeon.Core/Preparation/Msg.fs
@@ -0,0 +1,14 @@
+﻿namespace WhiteDungeon.Core.Preparation
+
+open WhiteDungeon.Core.Model
+open WhiteDungeon.Core.Game.Model
+
+
+type PlayerEdit =
+    | Occupation of Occupation
+
+
+type Msg =
+    | IncrPlayer
+    | DecrPlayer
+    | EditPlayer of Actor.PlayerID * PlayerEdit
---
+++ Dev/WhiteDungeon.Core/Preparation/Update.fs
@@ -0,0 +1,34 @@
+﻿module WhiteDungeon.Core.Preparation.Update
+
+open wraikny.Tart.Core
+
+open WhiteDungeon.Core
+
+
+let incrPlayer (model : Model) : Model =
+    let playerCount =
+        (model.playerCount + 1)
+        |> min model.gameSetting.maxPlayerCount
+
+    { model with
+        playerCount = playerCount
+        players =
+            let id = Game.Model.Actor.PlayerID (uint32 playerCount)
+            if model.players |> Map.containsKey id then
+                model.players
+            else
+                model.players
+                |> Map.add id ()
+    }
+
+
+let update (msg : Msg) (model : Model) : Model * Cmd<Msg, _> =
+    msg |> function
+    | IncrPlayer ->
+        incrPlayer model, Cmd.none
+    | DecrPlayer ->
+        { model with
+            playerCount =
+                (model.playerCount - 1)
+                |> min model.gameSetting.minPlayerCount
+        }, Cmd.none
