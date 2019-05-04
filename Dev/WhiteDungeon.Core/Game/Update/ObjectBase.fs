cbe5078e915c4540a8ce26ea206ababd6253b277
wraikny <writtenknown@gmail.com>
2019-05-05 00:47:34 +0900

    objectbase  move with dungeonmap inside

--- Dev/WhiteDungeon.Core/Game/Update/ObjectBase.fs
+++ Dev/WhiteDungeon.Core/Game/Update/ObjectBase.fs
@@ -1,6 +1,10 @@
 ﻿module WhiteDungeon.Core.Game.Update.ObjectBase
 
-
+open wraikny.Tart.Helper
+open wraikny.Tart.Helper.Math
+open wraikny.Tart.Helper.Geometry
+open wraikny.Tart.Advanced
+open WhiteDungeon.Core
 open WhiteDungeon.Core.Game.Model
 
 let setPosition position (obj : ObjectBase) = {
@@ -12,6 +16,33 @@ let setPosition position (obj : ObjectBase) = {
 
 let addPosition diff (obj : ObjectBase) =
     obj |> setPosition (diff + obj.position)
+
+
+let move
+    (gameSetting : Model.GameSetting)
+    (dungeonModel : Dungeon.DungeonModel)
+    (diff) (obj : ObjectBase)
+    =
+    let area = obj |> ObjectBase.area
+    let lu, rd = area |> Rect.get_LU_RD
+    let existsNextCell =
+        [lu; rd]
+        |> List.map(fun point ->
+            let nextPosition = point + diff
+            let nextCell =
+                Model.GameSetting.toDungeonCell
+                    gameSetting.dungeonCellSize
+                    nextPosition
+        
+            dungeonModel.cells
+            |> Map.containsKey nextCell
+        )
+        |> List.fold (&&) true
+    
+    if existsNextCell then
+        obj |> addPosition diff
+    else
+        obj
 
 
 let setVelocity velocity (obj : ObjectBase) = {
